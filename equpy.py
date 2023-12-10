f8a91cf3c0b105f5f478446440a876512f2501fb
mtod92 <marco.todisco.92@gmail.com>
2023-12-10 14:51:26 -0600

    added initial files

---
+++ equpy.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+
+class ChemicalReaction:
+    def __init__(self, species, stoichiometry, eq_constants, mass_conservation, initial_masses):
+        initial_masses = np.array(initial_masses, dtype=float)
+        if min(initial_masses) == 0:
+            warnings.warn("Species concentrations (S) should not be set to zero, eps has been set instead. The result may not be reliable.", stacklevel=2)
+            I = np.where(initial_masses == 0)[0]
+            initial_masses[I] = 2.2e-16
+    
+        self.s = species
+        self.N = stoichiometry
+        self.K = eq_constants
+        self.C = mass_conservation
+        self.S = initial_masses
+        self.result = []
+        self.residuals = []
+
+    def solve(self, iter, th, w):
+        iter = int(iter)        
+        x0 = np.ones(np.shape(self.N)[1])
+        delta = []
+
+        x, delta_ = eqsolver(self.N, self.K, self.C, self.S, x0, w)
+        delta.append(delta_)
+
+        for i in range(iter):
+            x, delta_ = eqsolver(self.N, self.K, self.C, self.S, x, w)
+            delta.append(delta_)
+
+            if delta[-1] < th*np.linalg.norm(x*2.2e-16):
+                self.result = np.exp(x)
+                self.residuals = delta
+                return np.exp(x), delta
+        
+        self.result = np.exp(x)
+        self.residuals = delta
+        warnings.warn("Tolerance not reached. Manually check if the result is satisfying. Either change starting point, increase iterations or weight.", stacklevel=2)
+        return np.exp(x), delta
+    
+    def plotter(self):
+        f, (ax1, ax2) = plt.subplots(1, 2, sharey=False, figsize=(15,5))
+        ax1.plot(np.arange(len(self.residuals)), self.residuals, lw=3, color='black')
+        ax1.scatter(np.arange(len(self.residuals)), self.residuals, s=120, color='black')
+
+        ax1.set_xlabel('steps', fontsize=12)
+        ax1.set_ylabel('||r||', fontsize=12)
+        ax1.set_yscale('linear')
+
+        ax2.bar(np.arange(0, len(self.result)), self.result)
+        ax2.set_ylabel('conc', fontsize=12)
+        ax2.set_xticks(np.arange(0, len(self.result)))
+        ax2.set_xticklabels(sorted(self.s, key=lambda x: self.s[x]), fontsize=12, rotation=90);
+
+def eqsolver(N, K, C, S, x, w):
+        Cx = C*np.exp(x)
+        W = Cx/np.sum(Cx, axis=1)[:,None]
+        Ks = np.prod((W/C)**W, axis=1)*S
+
+        M = np.vstack((N, W))
+        y = np.log(np.hstack((K, Ks)))
+        delta = np.linalg.norm(np.dot(M, x.T) - y[:,None].T)
+
+        if np.shape(N)[1] == len(K)+len(S):
+            x = (w*x + np.linalg.solve(M, y))/(w+1)
+        else: #solve overdetermined systems as M^(-1) * y
+            x = (w*x + np.dot(np.linalg.pinv(M), y))/(w+1)
+        return x, delta
---
+++ utils.py
@@ -0,0 +1,140 @@
+import re, csv
+import numpy as np
+from math import floor, log10
+
+def define_species_set(eq):
+    """
+    Extract species involved in the reaction from symbolic equations
+    Parameters:
+    - eq: list of equations in symbolic form
+    """
+    e_ = " ".join(eq)
+    e_ = e_.split(" ")
+
+    species_set = []
+
+    for l in e_:
+        if l != '+' and l != "=":
+            species_set.append(l)
+
+    for i, entry in enumerate(species_set):
+        if entry[0].isdigit():
+            entry_split = re.findall('\d+|\D+', entry)
+            species_set[i] = "".join(entry_split[1:])
+
+    return set(species_set)
+
+def define_reactions(eq):
+    """
+    Converts symbolic equations defining chemical reactions to matrix form suitable for the solver
+    Parameters:
+    - eq: list of equations in symbolic form
+    """
+      
+    species_set = define_species_set(eq)
+    n = len(species_set)
+    species = {string: index for index, string in enumerate(sorted(species_set))}
+
+    N = np.zeros((len(eq), n))
+
+    for i, eq_ in enumerate(eq):
+        eq_ = eq_.split('=')
+        eq_left = eq_[0].split('+')
+
+        for element in eq_left:
+            element = element.strip()
+            element_ = re.findall('\d+|\D+', element) #obtain blocks for every species
+
+            if len(element_) == 1:
+                N[i,species[element_[0]]] = -1
+            elif len(element_) > 1 and element_[0].isalpha():
+                N[i,species["".join(element_)]] = -1
+            else:
+                N[i,species["".join(element_[1:])]] = -int(element_[0])
+
+        eq_right = eq_[1].split('+')
+        for element in eq_right:
+            element = element.strip()
+            element_ = re.findall('\d+|\D+', element) #obtain blocks for every species
+
+            if len(element_) == 1:
+                N[i,species[element_[0]]] = 1
+            elif len(element_) > 1 and element_[0].isalpha():
+                N[i,species["".join(element_)]] = 1
+            else:
+                N[i,species["".join(element_[1:])]] = element_[0]
+
+    return N, n, species
+
+def define_conservations(eq, n, species):
+    """
+    Converts symbolic equations defining mass conservations to matrix form suitable for the solver
+    Parameters:
+    - eq: list of equations in symbolic form
+    - n: number of reactions
+    - species: dictionary containing {name: index} of the species involved in the reactions
+    """
+
+    pattern = r'(\d*\.?\d*)?([A-Za-z\d]+)'
+    M = np.zeros((len(eq), n))
+    eq = [eq_.replace("*", "") for eq_ in eq]
+
+    for i, eq_ in enumerate(eq):
+        eq_ = eq_.split('+')
+
+        for element_ in eq_:
+            element_ = re.findall(pattern, element_)
+            element_ = [elem for elem in element_[0]]
+            if len(element_[0]) == 0:
+                 M[i,species[element_[1]]] = 1
+            else:
+                M[i,species[element_[1]]] = element_[0]
+
+    return M
+
+def eq_system_builder(eq, mass_conservation):
+      """
+      Converts symbolic equations defining chemical reactions and mass conservations to matrix form suitable for the solver
+      Parameters:
+      - eq: list of reactions in symbolic form
+      - mass_conservation: list of mass conservations in symbolic form
+      """
+            
+      N, n, species = define_reactions(eq)
+      C = define_conservations(mass_conservation, n, species)
+
+      return N, C, species
+
+def csv_loader(filename_N, filename_C):
+
+    """
+    Assembles matrices suitable for the solver starting from .csv files
+    """
+          
+    N = []
+    K = []
+    C = []
+    S = []
+
+    with open(filename_N, newline='', encoding='utf-8-sig') as csvfile:
+        filecontent = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for i, row in enumerate(filecontent):
+            if i == 0:
+                lbls = row[0].split(',')[0:-1]
+            else:
+                N.append(row[0].split(',')[0:-1])
+                K.append(row[0].split(',')[-1])
+
+    with open(filename_C, newline='', encoding='utf-8-sig') as csvfile:
+        filecontent = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for i, row in enumerate(filecontent):
+            if i > 0:
+                C.append(row[0].split(',')[0:-1])
+                S.append(row[0].split(',')[-1])
+
+    N = np.array(N, dtype='float')
+    K = np.array(K, dtype='float')
+    C = np.array(C, dtype='float')
+    S = np.array(S, dtype='float')
+
+    return N, K, C, S
