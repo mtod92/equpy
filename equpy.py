c2d0e25ee4046a144a95c1a47af211d7d6f5c6ea
Luca Citelli <lcitelli@banor.it>
2023-12-12 16:40:29 +0100

    Black formatting

--- equpy.py
+++ equpy.py
@@ -2,13 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import warnings
 from typing import List
-#github test branch_1
+
+# github test branch_1
+
 
 class ChemicalReaction:
-    def __init__(self, species : List[str], stoichiometry, eq_constants, mass_conservation, initial_masses):
+    def __init__(
+        self,
+        species: List[str],
+        stoichiometry,
+        eq_constants,
+        mass_conservation,
+        initial_masses,
+    ):
         initial_masses = np.array(initial_masses, dtype=float)
         if min(initial_masses) == 0:
-            warnings.warn("Species concentrations (S) should not be set to zero, eps has been set instead. The result may not be reliable.", stacklevel=2)
+            warnings.warn(
+                "Species concentrations (S) should not be set to zero, eps has been set instead. The result may not be reliable.",
+                stacklevel=2,
+            )
             I = np.where(initial_masses == 0)[0]
             initial_masses[I] = 2.2e-16
 
@@ -21,7 +33,7 @@ class ChemicalReaction:
         self.residuals = []
 
     def solve(self, iter, th, w):
-        iter = int(iter)        
+        iter = int(iter)
         x0 = np.ones(np.shape(self.N)[1])
         delta = []
 
@@ -32,41 +44,49 @@ class ChemicalReaction:
             x, delta_ = eqsolver(self.N, self.K, self.C, self.S, x, w)
             delta.append(delta_)
 
-            if delta[-1] < th*np.linalg.norm(x*2.2e-16):
+            if delta[-1] < th * np.linalg.norm(x * 2.2e-16):
                 self.result = np.exp(x)
                 self.residuals = delta
                 return np.exp(x), delta
-        
+
         self.result = np.exp(x)
         self.residuals = delta
-        warnings.warn("Tolerance not reached. Manually check if the result is satisfying. Either change starting point, increase iterations or weight.", stacklevel=2)
+        warnings.warn(
+            "Tolerance not reached. Manually check if the result is satisfying. Either change starting point, increase iterations or weight.",
+            stacklevel=2,
+        )
         return np.exp(x), delta
-    
-    def plotter(self):
-        f, (ax1, ax2) = plt.subplots(1, 2, sharey=False, figsize=(15,5))
-        ax1.plot(np.arange(len(self.residuals)), self.residuals, lw=3, color='black')
-        ax1.scatter(np.arange(len(self.residuals)), self.residuals, s=120, color='black')
 
-        ax1.set_xlabel('steps', fontsize=12)
-        ax1.set_ylabel('||r||', fontsize=12)
-        ax1.set_yscale('linear')
+    def plotter(self):
+        f, (ax1, ax2) = plt.subplots(1, 2, sharey=False, figsize=(15, 5))
+        ax1.plot(np.arange(len(self.residuals)), self.residuals, lw=3, color="black")
+        ax1.scatter(
+            np.arange(len(self.residuals)), self.residuals, s=120, color="black"
+        )
+
+        ax1.set_xlabel("steps", fontsize=12)
+        ax1.set_ylabel("||r||", fontsize=12)
+        ax1.set_yscale("linear")
 
         ax2.bar(np.arange(0, len(self.result)), self.result)
-        ax2.set_ylabel('conc', fontsize=12)
+        ax2.set_ylabel("conc", fontsize=12)
         ax2.set_xticks(np.arange(0, len(self.result)))
-        ax2.set_xticklabels(sorted(self.s, key=lambda x: self.s[x]), fontsize=12, rotation=90);
+        ax2.set_xticklabels(
+            sorted(self.s, key=lambda x: self.s[x]), fontsize=12, rotation=90
+        )
+
 
 def eqsolver(N, K, C, S, x, w):
-        Cx = C*np.exp(x)
-        W = Cx/np.sum(Cx, axis=1)[:,None]
-        Ks = np.prod((W/C)**W, axis=1)*S
+    Cx = C * np.exp(x)
+    W = Cx / np.sum(Cx, axis=1)[:, None]
+    Ks = np.prod((W / C) ** W, axis=1) * S
 
-        M = np.vstack((N, W))
-        y = np.log(np.hstack((K, Ks)))
-        delta = np.linalg.norm(np.dot(M, x.T) - y[:,None].T)
+    M = np.vstack((N, W))
+    y = np.log(np.hstack((K, Ks)))
+    delta = np.linalg.norm(np.dot(M, x.T) - y[:, None].T)
 
-        if np.shape(N)[1] == len(K)+len(S):
-            x = (w*x + np.linalg.solve(M, y))/(w+1)
-        else: #solve overdetermined systems as M^(-1) * y
-            x = (w*x + np.dot(np.linalg.pinv(M), y))/(w+1)
-        return x, delta
+    if np.shape(N)[1] == len(K) + len(S):
+        x = (w * x + np.linalg.solve(M, y)) / (w + 1)
+    else:  # solve overdetermined systems as M^(-1) * y
+        x = (w * x + np.dot(np.linalg.pinv(M), y)) / (w + 1)
+    return x, delta
