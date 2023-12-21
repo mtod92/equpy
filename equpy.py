6c7ba10bb0994382b7d4bae607732c2b8920fb9d
mtod92 <marco.todisco.92@gmail.com>
2023-12-21 13:38:54 -0600

    edited equations class

--- equpy.py
+++ equpy.py
@@ -11,7 +11,7 @@ class EquationSystem:
             self.stoichiometry, self.mass_conservation, self.species = eq_system_builder(self.stoichiometry, self.mass_conservation)
 
         if self.species == None:
-            self.species = {'i':i for i in range(np.shape(N)[1])} #placeholder for species name if not specified
+            self.species = {'i':i for i in range(np.shape(self.stoichiometry)[1])} #placeholder for species name if not specified
 
 class ChemicalReaction:
     def __init__(
